a02820da5793b8e7b6414b7a7efc2d8f728d730e
diegodlpereira <diegodlpereira@gmail.com>
2019-01-31 22:23:03 +0100

    Project creation commit

---
+++ src/main/java/com/springframework/sfspetclinic/SfsPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.springframework.sfspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfsPetClinicApplication.class, args);
+    }
+
+}
+
