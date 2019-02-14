eceb2c6e5162ed46147315791f798b164668c1a9
diegodlpereira <diegodlpereira@gmail.com>
2019-02-14 20:16:00 +0100

    adding POJOs. Closes #1.

---
+++ src/main/java/com/springframework/sfspetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.sfspetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/springframework/sfspetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.sfspetclinic.model;
+
+public class Vet extends Person {
+}
