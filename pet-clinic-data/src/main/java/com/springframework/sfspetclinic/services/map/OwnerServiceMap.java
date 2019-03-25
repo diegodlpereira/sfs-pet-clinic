f5861fdc877e69d6215572bcca31d5d5c0b2b82a
diegodlpereira <diegodlpereira@gmail.com>
2019-03-25 22:37:21 +0100

    Implementing Map Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/springframework/sfspetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.springframework.sfspetclinic.services.map;
+
+import com.springframework.sfspetclinic.model.Owner;
+import com.springframework.sfspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.deleteByObject(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
