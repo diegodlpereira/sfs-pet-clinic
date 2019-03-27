8ce4a89bbad63efba8cf73f80218430eedd4b1fc
diegodlpereira <diegodlpereira@gmail.com>
2019-03-27 22:44:56 +0100

    Owner Index and Controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/com/springframework/sfspetclinic/controller/OwnersController.java
@@ -0,0 +1,17 @@
+package com.springframework.sfspetclinic.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+
+}
