8f8bc0919c8fdcda7d01dbe22b96cc0eb3610478
diegodlpereira <diegodlpereira@gmail.com>
2019-04-02 21:58:13 +0200

    Implementing simple logic to generate id automatically in the AbstractMapService. Closes #23.

--- pet-clinic-web/src/main/java/com/springframework/sfspetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sfspetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Joao");
         owner1.setLastName("Caldas");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Rafael");
         owner2.setLastName("Cysneiros");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners added..");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Laszlo");
         vet1.setLastName("Szabo");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Clovis");
         vet2.setLastName("Casoy");
 
