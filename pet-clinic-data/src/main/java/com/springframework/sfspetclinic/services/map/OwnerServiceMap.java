b4ce1334c8b4b73ab5f7becda9d364610c2b1e96
diegodlpereira <diegodlpereira@gmail.com>
2019-05-04 12:57:31 +0200

    Setting contact info and pets on startup. Needed to implement in the service implementations some checking against the subobjects needed to save the owner such as Pets and PetTypes in order to them before adding the new owner in order to get an ID. Closes #33

--- pet-clinic-data/src/main/java/com/springframework/sfspetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/sfspetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.springframework.sfspetclinic.services.map;
 
 import com.springframework.sfspetclinic.model.Owner;
+import com.springframework.sfspetclinic.model.Pet;
 import com.springframework.sfspetclinic.services.OwnerService;
+import com.springframework.sfspetclinic.services.PetService;
+import com.springframework.sfspetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +32,34 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        Owner savedOwner = null;
+
+        if(object != null){
+
+            if(object.getPets() != null){
+                // mimicking what hibernate would be supposed todo
+                object.getPets().forEach(pet -> {
+                    if(pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if(pet.getId() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+
+            return super.save(object);
+        }else {
+            return null;
+        }
+
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/springframework/sfspetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sfspetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springframework.sfspetclinic.bootstrap;
 
 import com.springframework.sfspetclinic.model.Owner;
+import com.springframework.sfspetclinic.model.Pet;
 import com.springframework.sfspetclinic.model.PetType;
 import com.springframework.sfspetclinic.model.Vet;
 import com.springframework.sfspetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.springframework.sfspetclinic.services.PetTypeService;
 import com.springframework.sfspetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Joao");
         owner1.setLastName("Caldas");
+        owner1.setAddress("Rua da Amizade, 123");
+        owner1.setCity("Recife");
+        owner1.setTelephone("32684998");
+
+        Pet joaosPet = new Pet();
+        joaosPet.setPetType(savedDogPetType);
+        joaosPet.setOwner(owner1);
+        joaosPet.setBirthDate(LocalDate.now());
+        joaosPet.setName("Procopio");
+
+        owner1.getPets().add(joaosPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Rafael");
         owner2.setLastName("Cysneiros");
+        owner2.setAddress("Rua do Habibs do Prado, 123");
+        owner2.setCity("Recife");
+        owner2.setTelephone("32323232");
+
+        Pet bobsCat = new Pet();
+        bobsCat.setPetType(savedCatPetType);
+        bobsCat.setOwner(owner2);
+        bobsCat.setBirthDate(LocalDate.now());
+        bobsCat.setName("Felicia");
 
         ownerService.save(owner2);
 
