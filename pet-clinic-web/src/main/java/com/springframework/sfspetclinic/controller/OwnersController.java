f9fb71e71d125e3d377b16f38e02292788c51a23
diegodlpereira <diegodlpereira@gmail.com>
2019-05-05 11:09:14 +0200

    Menu links corrected. NotImplemented Page create for showing pages under construnction. Closes #45

--- pet-clinic-web/src/main/java/com/springframework/sfspetclinic/controller/OwnersController.java
+++ pet-clinic-web/src/main/java/com/springframework/sfspetclinic/controller/OwnersController.java
@@ -24,4 +24,11 @@ public class OwnersController {
         return "owners/index";
     }
 
+    @RequestMapping({"/find"})
+    public String findOwners(){
+
+        return "notimplemented";
+    }
+
+
 }
