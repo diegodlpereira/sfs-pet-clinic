8ca4f96fb884bfef4cfc009bf14f6be541169a2f
diegodlpereira <diegodlpereira@gmail.com>
2020-02-22 17:10:05 +0100

    Quick refactor in order to test the Circle CI builds.

--- pet-clinic-data/src/test/java/com/springframework/sfspetclinic/repositories/OwnerRepositoryTestIT.java
+++ pet-clinic-data/src/test/java/com/springframework/sfspetclinic/repositories/OwnerRepositoryTestIT.java
@@ -2,11 +2,9 @@ package com.springframework.sfspetclinic.repositories;
 
 import com.springframework.sfspetclinic.model.Owner;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
@@ -33,6 +31,6 @@ public class OwnerRepositoryTestIT {
 
      Owner owner = ownerRepository.findByLastName("Caldas");
 
-     assertEquals("Caldas", owner);
+     assertEquals("Caldas", owner.getLastName());
     }
 }
