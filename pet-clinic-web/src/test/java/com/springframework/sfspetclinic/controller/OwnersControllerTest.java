76704551910ee78afde9c2f76459681b7dbbeeb5
diegodlpereira <diegodlpereira@gmail.com>
2020-02-22 12:52:44 +0100

    Leveraging some mockito features such as Argument Capture. Create Test with MockMVC to test requests in a unity, i.e. in a standalone class without setting up the Spring Context.

--- pet-clinic-web/src/test/java/com/springframework/sfspetclinic/controller/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/com/springframework/sfspetclinic/controller/OwnersControllerTest.java
@@ -1,17 +1,25 @@
 package com.springframework.sfspetclinic.controller;
 
+import com.springframework.sfspetclinic.model.Owner;
 import com.springframework.sfspetclinic.services.OwnerService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class OwnersControllerTest {
 
@@ -31,12 +39,36 @@ public class OwnersControllerTest {
     }
 
     @Test
-    public void listOwners() {
+    public void testMockMVC() throws Exception{
 
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(ownersController).build();
+
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"));
+
+    }
+
+    @Test
+    public void listOwners() throws Exception{
+
+        //given
+        Set<Owner> owners = new HashSet<>();
+        owners.add(new Owner());
+        owners.add(new Owner());
+
+        when(ownerService.findAll()).thenReturn(owners);
+
+        ArgumentCaptor<Set<Owner>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String listOwnersResult = ownersController.listOwners(model);
 
+        //then
         assertEquals("owners/index", listOwnersResult);
         verify(ownerService, times(1)).findAll();
-        verify(model, times(1)).addAttribute(eq("owners"), anySet());
+        verify(model, times(1)).addAttribute(eq("owners"), argumentCaptor.capture());
+        Set<Owner> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 }
